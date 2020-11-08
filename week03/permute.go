007b27cea4246f49d5ccbbdc661643f605a6461b
pipi666661 <pipi666661@126.com>
2020-11-09 00:54:21 +0800

    add permute

---
+++ week03/permute.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main(){
+	res := permute([]int{1,2,3})
+
+	for _,v := range res{
+		for _,q := range v{
+			fmt.Print(q)
+		}
+		fmt.Println()
+	}
+}
+
+
+func permute(nums []int) [][]int {
+	if len(nums) == 1 {
+		return [][]int{nums}
+	}
+
+	res := [][]int{}
+
+	for i, num := range nums {
+		// 把num从 nums 拿出去 得到tmp
+		tmp := make([]int, len(nums)-1)
+		copy(tmp[0:], nums[0:i])
+		copy(tmp[i:], nums[i+1:])
+
+		// sub 是把num 拿出去后，数组中剩余数据的全排列
+		sub := permute(tmp)
+		for _, s := range sub {
+			res = append(res, append(s, num))
+		}
+	}
+	return res
+}
+
+
